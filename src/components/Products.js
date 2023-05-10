952ba2a76d653fed734b7bada62a6ba9117f2abd
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-05-10 11:39:06 +0200

    dbRef & onSnapshot event

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,27 +1,42 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
 
+  //   useEffect(() => {
+  //     // Get the collection `products`
+  //     const dbRef = collection(db, "products");
+
+  //     getDocs(dbRef)
+  //       .then((qs) =>
+  //         setProducts(
+  //           qs.docs.map((doc) => {
+  //             return {
+  //               id: doc.id,
+  //               ...doc.data(),
+  //             };
+  //           })
+  //         )
+  //       )
+  //       .catch((err) => console.log(err));
+  //   }, []);
+
   useEffect(() => {
     // Get the collection `products`
     const dbRef = collection(db, "products");
 
-    getDocs(dbRef)
-      .then((qs) =>
-        setProducts(
-          qs.docs.map((doc) => {
-            return {
-              id: doc.id,
-              ...doc.data(),
-            };
-          })
-        )
+    const unsubscribe = onSnapshot(dbRef, (qs) =>
+      setProducts(
+        qs.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
       )
-      .catch((err) => console.log(err));
+    );
+    return () => unsubscribe();
   }, []);
 
   return (
