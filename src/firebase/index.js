7ae5a30d137bcef70307abae3627fb413d315acd
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-05-10 09:32:07 +0200

    firebase first setup (Google copy/paste)

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5MXh-fBRYHrieP3fG_SDOaLRNechqD0c",
+  authDomain: "mobile-2223-gb.firebaseapp.com",
+  projectId: "mobile-2223-gb",
+  storageBucket: "mobile-2223-gb.appspot.com",
+  messagingSenderId: "406478157623",
+  appId: "1:406478157623:web:180a596d55ffd0eb4cccb2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
