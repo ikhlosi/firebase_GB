04713f27ddfbbd58cd079fbbe1d0c1a604f546d7
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-05-10 11:26:25 +0200

    setProducts

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,23 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     // Get the collection `products`
     const dbRef = collection(db, "products");
 
-    getDocs(dbRef).then((qs) => {
-      qs.forEach((doc) => {
-        console.log(`doc.id => ${JSON.stringify(doc.data(), null, 2)}`);
-      });
-    });
+    getDocs(dbRef)
+      .then((qs) =>
+        setProducts(
+          qs.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          })
+        )
+      )
+      .catch((err) => console.log(err));
   }, []);
 
   return (
     <View>
       <Text>Products</Text>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => (
+          <Text>
+            {item.name} - {item.price}
+          </Text>
+        )}
+      />
     </View>
   );
 };
